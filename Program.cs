c28e5885a373dd0e9ce3f256796834580b196041
User <User@DESKTOP-ESP2JT1>
2022-11-09 15:51:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace New_Bilding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int count = int.Parse(Console.ReadLine());
+
+            for (int floor = floors; floor >= 1; floor--)
+            {
+                for (int number = 0; number < count; number++)
+                {
+                    string type = "";
+                    if (floor == floors)
+                    {
+                        type = "L";
+                    }
+                    else if (floor % 2 == 0)
+                    {
+                        type = "O";
+                    }
+                    else
+                    {
+                        type = "A";
+                    }
+                    Console.Write($"{type}{floor}{number} "); 
+                }
+               Console.WriteLine();
+            }
+        }
+    }
+}
